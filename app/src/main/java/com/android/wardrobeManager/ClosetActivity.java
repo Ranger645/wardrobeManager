0844ead0e085686ed8148b7d758205b2e68e2721
Matt Lips <mabzlips@gmail.com>
2019-09-29 20:57:26 -0400

    I made it so that the user is able to scroll through the closet clothing options vertically and be able to scroll through the individual clothing pieces horizontally through the categories. I also made a toggle so show or hide the view of the clothing types.

--- app/src/main/java/com/android/wardrobeManager/ClosetActivity.java
+++ app/src/main/java/com/android/wardrobeManager/ClosetActivity.java
@@ -1,31 +1,78 @@
 package com.android.wardrobeManager;
 
 import android.content.Intent;
+import android.opengl.Visibility;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class ClosetActivity extends AppCompatActivity {
+
+
+    boolean shirtsScrollVisible = false;
+    boolean shoesScrollVisible = false;
+    boolean shortsScrollVisible = false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_closet);
 
-        configureBackButton();
+    }
+
+    public void backToMainScreen(View view) {
+        startActivity(new Intent(ClosetActivity.this, StartActivity.class));
+    }
+
+    public void toggleShirtVisibility(View view) {
+
+        if (shirtsScrollVisible) {
+            shirtsScrollVisible = false;
+            findViewById(R.id.shirtScrollView).setVisibility(View.GONE);
+            ((TextView)findViewById(R.id.shirtsToggleView)).setText(">");
+        }
+        else {
+            shirtsScrollVisible = true;
+            findViewById(R.id.shirtScrollView).setVisibility(View.VISIBLE);
+            ((TextView)findViewById(R.id.shirtsToggleView)).setText("v");
+        }
+
 
     }
 
-    private void configureBackButton() {
-        Button closetButton = (Button) findViewById(R.id.closetBackButton);
+    public void toggleShoesVisibility(View view) {
 
-        closetButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(ClosetActivity.this, StartActivity.class));
-            }
-        });
+        if (shoesScrollVisible) {
+            shoesScrollVisible = false;
+            findViewById(R.id.shoesScrollView).setVisibility(View.GONE);
+            ((TextView)findViewById(R.id.shoesToggleView)).setText(">");
+        }
+        else {
+            shoesScrollVisible = true;
+            findViewById(R.id.shoesScrollView).setVisibility(View.VISIBLE);
+            ((TextView)findViewById(R.id.shoesToggleView)).setText("v");
+        }
+
+
+    }
+
+    public void toggleShortsVisibility(View view) {
+
+        if (shortsScrollVisible) {
+            shortsScrollVisible = false;
+            findViewById(R.id.shortsScrollView).setVisibility(View.GONE);
+            ((TextView)findViewById(R.id.shortsToggleView)).setText(">");
+        }
+        else {
+            shortsScrollVisible = true;
+            findViewById(R.id.shortsScrollView).setVisibility(View.VISIBLE);
+            ((TextView)findViewById(R.id.shortsToggleView)).setText("v");
+        }
+
+
     }
 
 }
