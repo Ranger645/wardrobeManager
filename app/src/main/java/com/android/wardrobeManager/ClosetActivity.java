a57d6cc4d2ffd5aefa8b97d7ae8a0bb83e929766
Matt Lips <mabzlips@gmail.com>
2019-10-13 14:05:34 -0400

    I created the new clothing item class and activity that connects with the expand closet activity

--- app/src/main/java/com/android/wardrobeManager/ClosetActivity.java
+++ app/src/main/java/com/android/wardrobeManager/ClosetActivity.java
@@ -1,11 +1,9 @@
 package com.android.wardrobeManager;
 
 import android.content.Intent;
-import android.opengl.Visibility;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
 public class ClosetActivity extends AppCompatActivity {
