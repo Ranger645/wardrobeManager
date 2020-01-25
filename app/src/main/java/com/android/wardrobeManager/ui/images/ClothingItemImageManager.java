98b280d17e1b6cbcc31c0b238a3d3852924516f6
gfoss <gmfoss645@gmail.com>
2020-01-25 15:41:02 -0500

    Laying down backend framework for adding custom images

--- app/src/main/java/com/android/wardrobeManager/ui/images/ClothingItemImageManager.java
+++ app/src/main/java/com/android/wardrobeManager/ui/images/ClothingItemImageManager.java
@@ -27,18 +27,48 @@ public class ClothingItemImageManager {
     final static String CLOTHING_ITEM_IMAGE_FOLDER = "clothing_item_images";
 
     public static Bitmap dynamicClothingItemLoad(Application application, ClothingItem toLoad) {
-        int hash = getImageHash(toLoad);
+        if (toLoad.isCustomImage()) {
+            String path = getClothingItemImagePath(Integer.toString(toLoad.getId()));
+            Bitmap bitmap = ImageIo.loadImageFromFile(path, application);
+            if (bitmap == null) {
+                Log.e("IMAGE_LOAD", "Failed to find custom image. Falling back to auto-generate.");
+                toLoad.setCustomImage(false);
+                bitmap = dynamicClothingItemLoad(application, toLoad);
+            }
+            return bitmap;
+        } else {
+            int hash = getImageHash(toLoad);
+            String imagePath = getClothingItemImagePath(Integer.toString(hash));
+            Bitmap bitmap = ImageIo.loadImageFromFile(imagePath, application);
+            if (bitmap != null)
+                return bitmap;
+
+            // Generating file manually and saving it to disc
+            bitmap = generateClothingItemImage(toLoad, application);
+            ImageIo.saveBitMapToFile(bitmap, imagePath, application);
+            return bitmap;
+        }
+    }
+
+    public static int getImageHash(ClothingItem toHash) {
+        StringBuilder builder = new StringBuilder();
+        builder.append(toHash.getSubType());
+        builder.append(toHash.getColors());
+        builder.append(toHash.getDesign());
+        return builder.toString().hashCode();
+    }
+
+    public static ClothingItem saveClothinItemCustomImage(ClothingItem item, Bitmap bitmap, Application application) {
+        item.setCustomImage(true);
+        ImageIo.saveBitMapToFile(bitmap, getClothingItemImagePath(Integer.toString(item.getId())), application);
+        return item;
+    }
+
+    private static String getClothingItemImagePath(String imageName) {
         StringBuilder pathBuilder = new StringBuilder(CLOTHING_ITEM_IMAGE_FOLDER);
         pathBuilder.append(File.pathSeparator);
-        pathBuilder.append(hash);
-        Bitmap bitmap = ImageIo.loadImageFromFile(pathBuilder.toString(), application);
-        if (bitmap != null)
-            return  bitmap;
-
-        // Generating file manually and saving it to disc
-        bitmap = generateClothingItemImage(toLoad, application);
-        ImageIo.saveBitMapToFile(bitmap, pathBuilder.toString(), application);
-        return bitmap;
+        pathBuilder.append(imageName);
+        return pathBuilder.toString();
     }
 
     private static Bitmap generateClothingItemImage(ClothingItem toLoad, Application application) {
@@ -53,16 +83,7 @@ public class ClothingItemImageManager {
         return ColorStyleFilterManager.filterColorStyle(toLoad.getDesign(), bitmap, reference, colors);
     }
 
-    public static int getImageHash(ClothingItem toHash) {
-        StringBuilder builder = new StringBuilder();
-        builder.append(toHash.getSubType());
-        builder.append(toHash.getColors());
-        builder.append(toHash.getDesign());
-        return builder.toString().hashCode();
-    }
-
     private static Bitmap drawableToBitmap (Drawable drawable) {
-
         if (drawable instanceof BitmapDrawable) {
             return ((BitmapDrawable)drawable).getBitmap();
         }
