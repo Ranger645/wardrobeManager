5fbfa84509ed51ec6d31f5c862d60ad4423ec6f7
gfoss <gmfoss645@gmail.com>
2020-01-27 13:08:02 -0500

    Chaning default scale type for color edit to center from xy

--- app/src/main/java/com/android/wardrobeManager/ui/color_edit/ColorEditPreview.java
+++ app/src/main/java/com/android/wardrobeManager/ui/color_edit/ColorEditPreview.java
@@ -9,6 +9,7 @@ import android.graphics.Rect;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.android.wardrobeManager.R;
 
@@ -26,22 +27,27 @@ public class ColorEditPreview extends View {
     private int touchX = -100, touchY = -100;
 
     private Bitmap imageBitmap;
-    private boolean customImage;
+    private ImageView.ScaleType scaleType = ImageView.ScaleType.FIT_XY;
 
     private ColorSelectListener colorSelectListener;
 
     public ColorEditPreview(Context context, Bitmap imageBitmap, boolean customImage) {
         super(context);
         this.imageBitmap = imageBitmap;
-        this.customImage = customImage;
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        Rect fullCanvas1 = new Rect(0, 0, canvas.getWidth(), canvas.getHeight());
+        Rect fullCanvas;
+        if (scaleType == ImageView.ScaleType.FIT_XY)
+            fullCanvas = new Rect(0, 0, canvas.getWidth(), canvas.getHeight());
+        else {
+            fullCanvas = new Rect(0, canvas.getHeight() / 2 - canvas.getWidth() / 2, canvas.getWidth(), canvas.getHeight() / 2 + canvas.getWidth() / 2);
+        }
+
         Paint defaultBrush = new Paint();
-        canvas.drawBitmap(imageBitmap, null, fullCanvas1, defaultBrush);
+        canvas.drawBitmap(imageBitmap, null, fullCanvas, defaultBrush);
 
         if (touchX >= 0 && touchY >= 0 && touchX < canvas.getWidth() && touchY < canvas.getHeight()) {
             Paint thickBrush = new Paint();
@@ -71,7 +77,7 @@ public class ColorEditPreview extends View {
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        if (customImage) {
+        if (scaleType == ImageView.ScaleType.FIT_XY) {
             if (selected) {
 
                 if (event.getAction() == MotionEvent.ACTION_MOVE || event.getAction() == MotionEvent.ACTION_DOWN) {
@@ -124,6 +130,10 @@ public class ColorEditPreview extends View {
         if ((color & 0xFF000000) == 0)
             return getResources().getColor(R.color.colorAccent, getContext().getTheme());
         return color;
+    }
+
+    public void setScaleType(ImageView.ScaleType scaleType) {
+        this.scaleType = scaleType;
     }
 
     public interface ColorSelectListener {
