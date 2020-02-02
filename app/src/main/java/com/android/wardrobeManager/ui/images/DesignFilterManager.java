bd8c19e7969e099ed306437644d5214aed306126
MattLips <39223894+MattLips@users.noreply.github.com>
2020-02-02 12:59:01 -0500

    I added the polka dots filter. (#15)
    * I changed the file names from using color to design. I changed the ClothingItem constructors to use @Ignore because it was giving me errors. I moved the primary_secondary filter to its own class and also added the thin and wide vertical and horizontal stripes.
    
    * I added the polka dots filter.
    
    * Made some changes from pull request

--- app/src/main/java/com/android/wardrobeManager/ui/images/DesignFilterManager.java
+++ app/src/main/java/com/android/wardrobeManager/ui/images/DesignFilterManager.java
@@ -9,6 +9,7 @@ public class DesignFilterManager {
 
     protected Map<String, DesignFilter> filters;
 
+
     private static DesignFilterManager designFilterManager = null;
 
     public static Bitmap filterDesign(String colorStyle, Bitmap ref, int[] colors) {
@@ -21,6 +22,7 @@ public class DesignFilterManager {
     public DesignFilterManager() {
         filters = new HashMap<>();
 
+        filters.put("polka_dots", new PolkaDots());
         filters.put("primary_secondary", new PrimarySecondary());
         filters.put("thin_stripes", new HorizontalStripes(10));
         filters.put("wide_stripes", new HorizontalStripes(80));
@@ -28,8 +30,6 @@ public class DesignFilterManager {
         filters.put("wide_horizontal_stripes", new HorizontalStripes(80));
         filters.put("thin_vertical_stripes", new VerticalStripes(10));
         filters.put("wide_vertical_stripes", new VerticalStripes(80));
-        filters.put("", new Default());
-        filters.put("null", new Default());
         filters.put("default", new Default());
 
     }
---
+++ app/src/main/java/com/android/wardrobeManager/ui/images/ThinVerticalStripes.java
@@ -0,0 +1,28 @@
+package com.android.wardrobeManager.ui.images;
+
+import android.graphics.Bitmap;
+
+import java.util.Map;
+
+public class ThinVerticalStripes implements DesignFilterManager.DesignFilter {
+
+    protected Map<String, DesignFilterManager.DesignFilter> filters;
+    private int thickness = 10;
+
+    public Bitmap filter(Bitmap bitmap, Bitmap ref, int[] colors) {
+
+
+        for (int i = 0; i < bitmap.getWidth() * bitmap.getHeight(); i++) {
+            int x = i % bitmap.getWidth();
+            int y = i / bitmap.getWidth();
+
+            if (ref.getPixel(x, y) != 0xFFFFFFFF) {
+                int stripeColor = colors[(x / thickness) % (colors.length)];
+
+                bitmap.setPixel(i % bitmap.getWidth(), i / bitmap.getWidth(), stripeColor);
+
+            }
+        }
+        return bitmap;
+    }
+}
