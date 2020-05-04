a9b3d104d2478795c721b28a6d4924c3a9c8f4ca
MattLips <39223894+MattLips@users.noreply.github.com>
2020-05-03 21:32:03 -0400

    Closet sort by (#28)
    * Hamberger button and closet menu fragment
    
    * Closet menu transition
    
    * Hamburger button opens menu
    
    * Sort By Fragment
    
    * Closet activity fix
    
    * made changes to fix bugs after merge
    
    * Fixed sort by button saving
    
    * Pull request fixes

--- app/src/main/java/com/android/wardrobeManager/ui/closet/ClosetActivity.java
+++ app/src/main/java/com/android/wardrobeManager/ui/closet/ClosetActivity.java
@@ -26,10 +26,30 @@ public class ClosetActivity extends AppCompatActivity implements ClosetClothingI
     private ClosetShadeFragment clickableShade;
     private ClosetShadeFragment nonClickableShade;
     private ClosetMenuFragment closetMenu;
+    private ClosetSortByFragment sortBy;
     private FragmentTransaction fragmentTransaction;
-    private View closetMenuButton;
+
     private boolean menuIsOpen = false;
+    private boolean sortByIsOpen = false;
+
+    private int sortingType = 0;
+
+    private ClosetShadeFragment sortByTopShade;
+    private ClosetShadeFragment sortByLeftShade;
+    private ClosetShadeFragment sortByBottomShade;
+    private ClosetShadeFragment sortByRightShade;
+    private ClosetShadeFragment sortByMiddleShade;
+
+    private View closetMenuButton;
     private View clickableShadeView;
+    private View sortByTopShadeView;
+    private View sortByLeftShadeView;
+    private View sortByBottomShadeView;
+    private View sortByRightShadeView;
+    private View sortByLaundryToggle;
+    private View sortByAlphabeticalToggle;
+    private View sortByColorToggle;
+    private View sortBySizeToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,10 +62,23 @@ public class ClosetActivity extends AppCompatActivity implements ClosetClothingI
 
         clickableShade = new ClosetShadeFragment();
         nonClickableShade = new ClosetShadeFragment();
+        sortBy = new ClosetSortByFragment();
         closetMenu = new ClosetMenuFragment();
+
+        sortByTopShade = new ClosetShadeFragment();
+        sortByLeftShade = new ClosetShadeFragment();
+        sortByBottomShade = new ClosetShadeFragment();
+        sortByRightShade = new ClosetShadeFragment();
+        sortByMiddleShade = new ClosetShadeFragment();
+
         closetMenuButton = findViewById(R.id.closet_menu_button);
+
         clickableShadeView = findViewById(R.id.clickable_shade);
-        clickableShadeView.setClickable(false);
+        sortByTopShadeView = findViewById(R.id.sort_by_top_clickable_shade);
+        sortByLeftShadeView = findViewById(R.id.sort_by_left_clickable_shade);
+        sortByBottomShadeView = findViewById(R.id.sort_by_bottom_clickable_shade);
+        sortByRightShadeView = findViewById(R.id.sort_by_right_clickable_shade);
+        updateShadeViewClickable();
 
         final ClosetAdapter adapter = new ClosetAdapter(this);
         recyclerView.setAdapter(adapter);
@@ -70,7 +103,8 @@ public class ClosetActivity extends AppCompatActivity implements ClosetClothingI
         if (view.getId() == findViewById(R.id.closet_menu_button).getId()) {
             if (!closetMenu.isFragmentOpen() && !nonClickableShade.isFragmentOpen() && !clickableShade.isFragmentOpen()) {
                 menuIsOpen = true;
-                clickableShadeView.setClickable(true);
+                updateShadeViewClickable();
+
                 fragmentTransaction = getSupportFragmentManager().beginTransaction();
                 fragmentTransaction.setCustomAnimations(R.anim.fade_in, R.anim.fade_out);
                 fragmentTransaction.add(R.id.shade_layout_holder, clickableShade).commit();
@@ -91,9 +125,33 @@ public class ClosetActivity extends AppCompatActivity implements ClosetClothingI
         }
     }
 
+    public void onSortByClicked(View view) {
+        if (view.getId() == findViewById(R.id.closet_menu_sort_by_button).getId() && closetMenu.isFragmentOpen() && nonClickableShade.isFragmentOpen() && clickableShade.isFragmentOpen()) {
+            closeMenu();
+            menuIsOpen = false;
+            sortByIsOpen = true;
+            updateShadeViewClickable();
+
+            fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.setCustomAnimations(R.anim.slide_extended_towards_right, R.anim.fade_out);
+            fragmentTransaction.add(R.id.sort_by_holder, sortBy);
+            fragmentTransaction.commit();
+
+            fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.setCustomAnimations(R.anim.fade_in, R.anim.fade_out);
+            fragmentTransaction.add(R.id.sort_by_top_clickable_shade, sortByTopShade);
+            fragmentTransaction.add(R.id.sort_by_left_clickable_shade, sortByLeftShade);
+            fragmentTransaction.add(R.id.sort_by_bottom_clickable_shade, sortByBottomShade);
+            fragmentTransaction.add(R.id.sort_by_right_clickable_shade, sortByRightShade);
+            fragmentTransaction.add(R.id.sort_by_middle_unclickable_shade, sortByMiddleShade);
+            fragmentTransaction.commit();
+            findViewById(R.id.closet_menu_button).setVisibility(View.INVISIBLE);
+        }
+    }
+
     @Override
     public void onClothingItemClick(ClothingItem item, ImageButton clothingImageView) {
-        if (!menuIsOpen)  {
+        if (!menuIsOpen && !sortByIsOpen)  {
             Intent intent = new Intent(ClosetActivity.this, AddItemActivity.class);
             Bundle bundle = new Bundle();
             bundle.putParcelable("clothingItem", item);
@@ -105,8 +163,49 @@ public class ClosetActivity extends AppCompatActivity implements ClosetClothingI
 
     public void shadeClicked(View view) {
         if (menuIsOpen) {
+            closeMenu();
+            updateShadeViewClickable();
+            findViewById(R.id.closet_menu_button).setVisibility(View.VISIBLE);
+            Animation slideMenuButton = AnimationUtils.loadAnimation(this, R.anim.wait_then_slide_from_right);
+            closetMenuButton.startAnimation(slideMenuButton);
+        } else if (sortByIsOpen) {
+            sortByIsOpen = false;
+            updateShadeViewClickable();
+
+            fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.setCustomAnimations(R.anim.slide_extended_away_left, R.anim.slide_extended_away_left);
+            fragmentTransaction.remove(sortBy);
+            fragmentTransaction.commit();
+
+            fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.setCustomAnimations(R.anim.fade_in, R.anim.fade_out);
+            fragmentTransaction.remove(sortByTopShade).commit();
+
+            fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.setCustomAnimations(R.anim.fade_in, R.anim.fade_out);
+            fragmentTransaction.remove(sortByLeftShade).commit();
+
+            fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.setCustomAnimations(R.anim.fade_in, R.anim.fade_out);
+            fragmentTransaction.remove(sortByBottomShade).commit();
+
+            fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.setCustomAnimations(R.anim.fade_in, R.anim.fade_out);
+            fragmentTransaction.remove(sortByRightShade).commit();
+
+            fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.setCustomAnimations(R.anim.fade_in, R.anim.fade_out);
+            fragmentTransaction.remove(sortByMiddleShade).commit();
+
+            Animation slideMenuButton = AnimationUtils.loadAnimation(this, R.anim.wait_then_slide_from_right);
+            findViewById(R.id.closet_menu_button).startAnimation(slideMenuButton);
+            findViewById(R.id.closet_menu_button).setVisibility(View.VISIBLE);
+        }
+    }
+
+    public void closeMenu() {
+        if (menuIsOpen) {
             menuIsOpen = false;
-            clickableShadeView.setClickable(false);
             fragmentTransaction = getSupportFragmentManager().beginTransaction();
             fragmentTransaction.setCustomAnimations(R.anim.fade_in, R.anim.fade_out);
             fragmentTransaction.remove(clickableShade).commit();
@@ -116,14 +215,46 @@ public class ClosetActivity extends AppCompatActivity implements ClosetClothingI
             fragmentTransaction.remove(nonClickableShade).commit();
 
             fragmentTransaction = getSupportFragmentManager().beginTransaction();
-            fragmentTransaction.setCustomAnimations(R.anim.slide_from_right, R.anim.slide_away_right);
-            fragmentTransaction.remove(closetMenu);
-            fragmentTransaction.commit();
-
-            Animation slideMenuButton = AnimationUtils.loadAnimation(this, R.anim.wait_then_slide_from_right);
-            closetMenuButton.startAnimation(slideMenuButton);
-            closetMenuButton.setVisibility(View.VISIBLE);
+            fragmentTransaction.setCustomAnimations(R.anim.fade_in, R.anim.slide_away_right);
+            fragmentTransaction.remove(closetMenu).commit();
         }
     }
 
+    public void sortByToggleClicked(View view) {
+            sortByLaundryToggle = findViewById(R.id.laundryStatusButton);
+            sortByAlphabeticalToggle = findViewById(R.id.alphabeticalButton);
+            sortByColorToggle = findViewById(R.id.colorButton);
+            sortBySizeToggle = findViewById(R.id.sizeButton);
+
+            sortByLaundryToggle.setBackground(getDrawable(R.drawable.binary_button_deselected));
+            sortByAlphabeticalToggle.setBackground(getDrawable(R.drawable.binary_button_deselected));
+            sortByColorToggle.setBackground(getDrawable(R.drawable.binary_button_deselected));
+            sortBySizeToggle.setBackground(getDrawable(R.drawable.binary_button_deselected));
+
+            if (sortByLaundryToggle == view) {
+                sortByLaundryToggle.setBackground(getDrawable(R.drawable.binary_button_selected));
+                sortingType = 1;
+            } else if (sortByAlphabeticalToggle == view) {
+                sortByAlphabeticalToggle.setBackground(getDrawable(R.drawable.binary_button_selected));
+                sortingType = 2;
+            } else if (sortByColorToggle == view) {
+                sortByColorToggle.setBackground(getDrawable(R.drawable.binary_button_selected));
+                sortingType = 3;
+            } else if (sortBySizeToggle == view) {
+                sortBySizeToggle.setBackground(getDrawable(R.drawable.binary_button_selected));
+                sortingType = 4;
+            }
+    }
+
+    public void updateShadeViewClickable() {
+        clickableShadeView.setClickable(menuIsOpen);
+        sortByTopShadeView.setClickable(sortByIsOpen);
+        sortByLeftShadeView.setClickable(sortByIsOpen);
+        sortByBottomShadeView.setClickable(sortByIsOpen);
+        sortByRightShadeView.setClickable(sortByIsOpen);
+    }
+
+    public int getSortingType() {
+        return sortingType;
+    }
 }
